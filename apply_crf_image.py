db197e13f053930f036bf8583c39c0ec50f0d009
AruniRC <arunirc@cs.umass.edu>
2017-11-04 13:34:53 -0400

    test apply_crf_image on samples

--- apply_crf_image.py
+++ apply_crf_image.py
@@ -7,11 +7,16 @@
     - Alternatively, you can call this script from the cmd line and pass the args:
         > python apply_crf.py -i IMAGE_DATASET -o SEG_DATASET -o data/crf-output
     - Optional: modify path to Python interpreter in the first line of this script.
+
+    - Usage:
+        python apply_crf_image.py -i INPUT_IMAGE_FILE -s INPUT_SEG_MAT_FILE -o OUTPUT_MAT_FILE
 '''
-IMAGE_PATH = '/data2/arunirc/Research/FlowNet2/flownet2-docker/data/complexBackground/complexBackground-multilabel/'
-SEG_PATH = '/data/pbideau/motionSegmentation/Aruni_CRF/complex-background-multi-labels/'
-IMAGE_EXT = ['.jpg', '.png']
-OUT_PATH = 'data/crf-output/complex-bg'
+
+from __future__ import division
+
+IMAGE_PATH = './samples/bear01/bear01_0002.jpg'
+SEG_PATH = './samples/bear01/00002.mat'
+OUT_PATH = './samples/bear01/crf_seg.mat'
 
 
 import pydensecrf.densecrf as dcrf
@@ -28,49 +33,22 @@ from os import listdir, makedirs
 from os.path import isfile, join, isdir
 import argparse
 
-import traceback
-import warnings
 import sys
 
 
 def parse_input_opts():
     parser = argparse.ArgumentParser(description='Run dense-crf on single frame motion segmentations')
     parser.add_argument('-i', '--image', help='Specify path to RGB image', \
-                            default=IMAGE_DATA)
+                            default=IMAGE_PATH)
     parser.add_argument('-s', '--seg', help='Specify path to raw segmentations (HxWxK .mat file)', \
-                            default=SEG_DATA)
+                            default=SEG_PATH)
     parser.add_argument('-o', '--out', help='Specify output path for CRF segmentations', \
-                            default=OUT_DIR)
+                            default=OUT_PATH)
     parser.add_argument('-v', '--viz', help='Save visualized original and CRF segmentations as images', \
                             default=False, action='store_true')
-    parser.add_argument('-cgw', '--crf_gaussian_weight', help='CRF weight for pairwise Gaussian term', \
-                            default=3)
-    parser.add_argument('-cbw', '--crf_bilateral_weight', help='CRF weight for pairwise bilateral term', \
-                            default=5)
-    parser.add_argument('-bsx', '--bilateral_sx', help='x_stdev for pairwise bilateral term', \
-                            default=50)
-    parser.add_argument('-bsy', '--bilateral_sy', help='y_stdev for pairwise bilateral term', \
-                            default=50)
-    parser.add_argument('-bsc', '--bilateral_color', help='CRF weight for pairwise bilateral term', \
-                            default=10)
+
     opts = parser.parse_args()
-    opts.image_exts = IMAGE_EXT
-    opts.crf_gaussian_weight = int(opts.crf_gaussian_weight)
-    opts.crf_bilateral_weight = int(opts.crf_bilateral_weight)
-    opts.bilateral_sx = int(opts.bilateral_sx)
-    opts.bilateral_sy = int(opts.bilateral_sy)
-    opts.bilateral_color = int(opts.bilateral_color)
     return opts
-
-
-# DEBUG
-def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-    # diagnose warning tracebacks
-    log = file if hasattr(file,'write') else sys.stderr
-    traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
-
-# warnings.showwarning = warn_with_traceback # // Uncomment to traceback warnings
 
 
 
@@ -91,7 +69,6 @@ def preprocess_label_scores(res):
     res_sum = np.squeeze(np.sum(np.sum(res, axis=0), axis=0))
     label_map = np.array(np.where(res_sum > 0)[0])
     labels = np.squeeze(res[:,:,label_map]) # keep non-zero presence labels
-    # from IPython.core.debugger import Tracer; Tracer()()
     # assert np.abs(np.sum(labels) - res.shape[0]*res.shape[1]) < np.finfo(np.float32).eps # check probability
     labels = np.transpose(labels, (2,0,1)) # C x H x W
     n_labels = len(label_map)
@@ -106,13 +83,13 @@ def get_crf_seg(img, labels, n_labels, opts):
     U = unary_from_softmax(labels)
     crf.setUnaryEnergy(U)
     feats = create_pairwise_gaussian(sdims=(3, 3), shape=img.shape[:2])
-    crf.addPairwiseEnergy(feats, compat=opts.crf_gaussian_weight,
+    crf.addPairwiseEnergy(feats, compat=3,
                     kernel=dcrf.DIAG_KERNEL,
                     normalization=dcrf.NORMALIZE_SYMMETRIC)
-    feats = create_pairwise_bilateral(sdims=(opts.bilateral_sx, opts.bilateral_sy), \
-                                      schan=(opts.bilateral_color, opts.bilateral_color, opts.bilateral_color),
-                                  img=img, chdim=2)
-    crf.addPairwiseEnergy(feats, compat=opts.crf_bilateral_weight,
+    feats = create_pairwise_bilateral(sdims=(50, 50), \
+                                      schan=(10, 10, 10),
+                                      img=img, chdim=2)
+    crf.addPairwiseEnergy(feats, compat=5,
                     kernel=dcrf.DIAG_KERNEL,
                     normalization=dcrf.NORMALIZE_SYMMETRIC)
     Q = crf.inference(5)
@@ -121,73 +98,76 @@ def get_crf_seg(img, labels, n_labels, opts):
 
 
 
+def apply_crf_seg_img(opts):
 
-def apply_crf_seg(opts):
+    # Read Motion Segmentation result (MAT file)
+    seg_file = opts.seg
+    
+    if not isfile(seg_file):
+        print 'Input motion segmentation file not found.'
+        return
+
+    mat_data = sio.loadmat(seg_file)
+    res = mat_data['objectProb']
+
+    # check `res` dims
+    if res.ndim != 3:
+        # only background class predicted in raw segmentation: 
+        #   -- stack `(1-res)` onto 3rd axis
+        res = np.stack((res,1-res), axis=2) 
+    
+
+    # Read RGB image
+    if not isfile(opts.image):
+        print 'Input RGB image file not found.'
+        return
+    img = imread(opts.image)
+
+
+
+    # Input probabilities (unary terms)
+    labels, label_map, n_labels = preprocess_label_scores(res)
+    
+
+    # Dense CRF inference
+    Q = get_crf_seg(img, labels, n_labels, opts)
+    
+    # CRF scores into Pia's format
+    probQ = np.array(Q) 
+    crf_prob = probQ.reshape((probQ.shape[0], img.shape[0] ,img.shape[1]))
+    crf_prob = crf_prob.transpose((1,2,0))
+    crf_out_final = np.zeros(res.shape)
+    crf_out_final[:,:,label_map] = crf_prob
 
     
-                
-                # Read Motion Segmentation result (MAT file)
-                #   -- assuming that you consistently zero-pad your output seg files
-                seg_file = join(seg_dir, frame_num.zfill(5) + '.mat')
-                
-                if not isfile(seg_file):
-                    print 'Input motion segmentation file not found.'
-                    return
+    # Save as matlab MATLAB-style .mat file
+    sio.savemat(opts.out, dict(objectProb=crf_out_final))
+    
 
-                mat_data = sio.loadmat(seg_file)
-                res = mat_data['objectProb']
+    # Optional: save labelings as images
+    if opts.viz:
+        # CRF MAP labels
+        MAP = np.argmax(Q, axis=0) 
+        crf_map_img = MAP.reshape((img.shape[0], img.shape[1]))
+        crf_label_img = label_map[crf_map_img] # CRF labels --> instance labels
+        
+        # side-by-side visualizations as RGB overlays
+        crf_rgb = color.label2rgb(crf_label_img, img, bg_label=0)
+        res_label =  np.argmax(res, axis=2)
+        res_rgb = color.label2rgb(res_label, img, bg_label=0)
+        tiled_img = np.concatenate((res_rgb, \
+                        np.zeros([res_rgb.shape[0],10,3]), \
+                        crf_rgb), axis=1)
 
-                # check `res` dims
-                if res.ndim != 3:
-                    from IPython.Debugger import Tracer; Tracer()()
-                    # only background class predicted: save as-is w/o CRF
-                    sio.savemat(join(opts.out_dir, d, frame_num.zfill(5) + '.mat'), \
-                            dict(objectProb=res))
-                    continue 
-                
-                # read RGB image
-                img = imread(join(vid_dir, frame))
-
-                # DEBUG
-                # from IPython.core.debugger import Tracer; Tracer()()
-
-                # input probabilities (unary terms)
-                labels, label_map, n_labels = preprocess_label_scores(res)
-                
-                # Dense CRF inference
-                Q = get_crf_seg(img, labels, n_labels, opts)
-                
-                probQ = np.array(Q) # CRF scores into Pia's format
-                crf_prob = probQ.reshape((probQ.shape[0], img.shape[0] ,img.shape[1]))
-                crf_prob = crf_prob.transpose((1,2,0))
-                crf_out_final = np.zeros(res.shape)
-                crf_out_final[:,:,label_map] = crf_prob
-
-                
-                # save as matlab MATLAB-style .mat file
-                sio.savemat(join(opts.out_dir, d, frame_num.zfill(5) + '.mat'), \
-                            dict(objectProb=crf_out_final))
-                
-                if opts.viz:
-                    # CRF MAP labels
-                    MAP = np.argmax(Q, axis=0) 
-                    crf_map_img = MAP.reshape((img.shape[0], img.shape[1]))
-                    crf_label_img = label_map[crf_map_img] # CRF labels --> instance labels
-                    
-                    # side-by-side visualizations as RGB overlays
-                    crf_rgb = color.label2rgb(crf_label_img, img)
-                    res_label =  np.argmax(res, axis=2)
-                    res_rgb = color.label2rgb(res_label, img)
-                    tiled_img = np.concatenate((res_rgb, \
-                                    np.zeros([res_rgb.shape[0],10,3]), \
-                                    crf_rgb), axis=1)
-                    imsave(join(opts.out_dir, d, 'viz', frame_num+'_raw_crf.png'), \
-                                    tiled_img)                
+        # save tiled image
+        out_dir, _ = os.path.split(opts.out)
+        _, fn = os.path.split(opts.image)
+        imsave(join(out_dir, 'viz_raw_crf_'+fn), tiled_img)
 
 
 # entry point
 if __name__ == '__main__':
 
     opts = parse_input_opts()
-    apply_crf_seg(opts)
+    apply_crf_seg_img(opts)
     
